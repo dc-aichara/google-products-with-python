7c515086ffbf60b4fd1cdc0ee6432e4b1f2cdbcb
Dayal Chand Aichara <dc.aichara@lastroots.com>
2019-06-12 16:30:10 +0900

    Update

--- bq.py
+++ bq.py
@@ -5,8 +5,8 @@ from Google BigQuery as pandas frame .
 import pandas_gbq
 
 PROJECT_ID = "Your Project ID"
-data_Set = "bigquery-public-data"  # Replace this with your data set name
-sub_data_set = "ml_datasets"   # Replace this with your sub  data set name
+database = "bigquery-public-data"  # Replace this with your database name
+data_set = "ml_datasets"   # Replace this with your data set name
 data_table = "census_adult_income"  # Replace this with your table name
 
 
@@ -17,7 +17,7 @@ FROM
   [{0}:{1}.{2}]
 """
 
-query = query.format(data_Set, sub_data_set, data_table)
+query = query.format(database, data_set, data_table)
 
 df = pandas_gbq.read_gbq(query, project_id=PROJECT_ID, dialect="legacy")
 
