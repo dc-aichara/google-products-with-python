5fe3d57922b8ae91641d8aab694d8e311d296288
Dayal Chand Aichara <dc.aichara@lastroots.com>
2019-06-12 13:59:47 +0900

    BigQuery Python code added

--- bq.py
+++ bq.py
@@ -1,8 +1,9 @@
 """
-This python script uses pandas_gbq python package to extract data as pandas dataframe.
+This python script uses pandas_gbq python package to extract data
+from Google BigQuery as pandas frame .
 """
-
 import pandas_gbq
+
 PROJECT_ID = "Your Project ID"
 data_Set = "bigquery-public-data"  # Replace this with your data set name
 sub_data_set = "ml_datasets"   # Replace this with your sub  data set name
@@ -15,6 +16,7 @@ SELECT
 FROM
   [{0}:{1}.{2}]
 """
+
 query = query.format(data_Set, sub_data_set, data_table)
 
 df = pandas_gbq.read_gbq(query, project_id=PROJECT_ID, dialect="legacy")
